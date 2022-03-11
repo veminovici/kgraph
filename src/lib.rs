a96183e8311b114ffb92737834be007a17c22fb7
VLAD EMINOVICI <veminovici@hotmail.com>
2022-03-11 15:15:45 +0200

    Initial project integrated with quickcheck

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use quickcheck_macros::quickcheck;
+
+    #[quickcheck]
+    fn opposite_is_diff(b: bool) -> bool {
+        let x = !b;
+        x != b
+    }
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
